6515ae3c905da4095ef2adab02ece63fc639c2f1
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-03-01 01:49:27 +0100

    CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
     let dst = Config::new("HiGHS")
         .define("FAST_BUILD", "ON")
         .define("SHARED", "OFF")
-        .define("MSVC_RUNTIME_LIBRARY", "MultiThreadedDLL")
+        .define("CMAKE_MSVC_RUNTIME_LIBRARY", "MultiThreadedDLL")
         .build();
 
     let include_path = dst.join("include");
