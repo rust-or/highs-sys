c8d4213153422e81f983493fc84cd47f34653491
Tobias Zimmermann <cu3po42@gmail.com>
2023-09-14 22:58:32 +0200

    Enable system-installed HiGHS to be linked (#20)
    * Enable system-installed HiGHS to be linked
    
    Instead of always building HiGHS, also allow for a version already
    installed on the system (e.g. via a distro package manager) to be
    linked.
    
    * Add missing link in README.md
    
    * Use new syntax for optional dependencies
    
    * Add CI test for 'discover' feature on Linux and macOS

--- build.rs
+++ build.rs
@@ -1,9 +1,40 @@
 use std::env;
-use std::path::PathBuf;
+use std::fs::write;
+use std::path::{PathBuf, Path};
 
-use cmake::Config;
+fn generate_bindings<'a>(include_paths: impl Iterator<Item = &'a Path>) {
+    // First, we write a trivial wrapper header so that the HiGHS headers can be discovered from
+    // the include path.
+    let mut wrapper_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    wrapper_path.push("highs.h");
+    write(&wrapper_path, "#include <interfaces/highs_c_api.h>").unwrap();
 
-fn main() {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = include_paths.fold(bindgen::Builder::default(), |builder, path| builder.clang_arg(format!("-I{}", path.to_string_lossy())));
+    let c_bindings = builder
+        // The input header we would like to generate
+        // bindings for.
+        .header(wrapper_path.to_string_lossy())
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    c_bindings
+        .write_to_file(out_path.join("c_bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
+#[cfg(feature = "build")]
+fn build() -> bool {
+    use cmake::Config;
     let mut dst = Config::new("HiGHS");
 
     if cfg!(feature = "ninja") {
@@ -23,37 +54,7 @@ fn main() {
         .build();
 
     let include_path = dst.join("include").join("highs");
-    let src_path = PathBuf::from("HiGHS").join("src");
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let c_bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header(
-            include_path
-                .join("interfaces")
-                .join("highs_c_api.h")
-                .to_string_lossy(),
-        )
-        .clang_args(&[
-            &format!("-I{}", include_path.to_string_lossy()),
-            &format!("-I{}", src_path.to_string_lossy()),
-        ])
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    c_bindings
-        .write_to_file(out_path.join("c_bindings.rs"))
-        .expect("Couldn't write bindings!");
+    generate_bindings(Some(include_path.as_path()).into_iter());
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-search=native={}/lib64", dst.display());
@@ -79,4 +80,40 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=gomp");
     }
     println!("cargo:rerun-if-changed=HiGHS/src/interfaces/highs_c_api.h");
+
+    true
+}
+
+#[cfg(not(feature = "build"))]
+fn build() -> bool {
+    false
+}
+
+#[cfg(feature = "discover")]
+fn discover() -> bool {
+    let lib = match pkg_config::Config::new().atleast_version("1.5.0").probe("highs") {
+        Ok(lib) => lib,
+        Err(_e) => return false,
+    };
+
+    generate_bindings(lib.include_paths.iter().map(|p| p.as_path()));
+
+   true 
+
+}
+
+#[cfg(not(feature = "discover"))]
+fn discover() -> bool {
+    false
+}
+
+fn main() {
+    if cfg!(all(any(feature = "highs_release", feature = "libz", feature = "ninja"), not(feature = "build"))) {
+        panic!("You have enabled features that control how HiGHS is built, but have not enabled the 'build' feature.\n\
+               Thus, your features will never have any effect. Please enable the 'build' feature on highs-sys if you want to build HiGHS or disable the 'libz', 'ninja' and 'highs_release' features.");
+    }
+
+    if !discover() && !build() {
+        panic!("Could neither discover nor build HiGHS");
+    }
 }
